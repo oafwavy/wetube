19cb75f5f701037254e62d5939ebe6a6814c335c
oafwavy <oaf.wavy@gmail.com>
2020-01-24 23:47:04 +0900

    add controller;

--- app.js
+++ app.js
@@ -10,15 +10,11 @@ import routes from "./routes";
 
 
 const app = express()
-
-
-
 app.use(cookieParser());
+app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json({}));
-
 
 app.use(helmet());
 app.use(morgan("dev"));
